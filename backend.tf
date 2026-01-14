58cca6e5c0f630ee220867e364e89a620824d920
Md Irfan <145644646+Irfannoida162@users.noreply.github.com>
2026-01-14 12:16:33 +0530

    Fix region in S3 backend configuration
    availability zone update

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "demo-buckey-github123"
     key            = "vpc/terraform.tfstate"
-    region         = "ap-south-1a"
+    region         = "ap-south-1"
     encrypt        = true
     use_lockfile   = true 
   }
